e097537606e84c4ea939cf17a8dba2d6535af2c3
Fran casa <fsannicceb@educacion.navarra.es>
2025-04-27 04:24:17 +0200

    Creacion de Before y After each

--- Pruebas_Junit_2/src/test/java/org/example/CalculadoraTest.java
+++ Pruebas_Junit_2/src/test/java/org/example/CalculadoraTest.java
@@ -1,10 +1,25 @@
 package org.example;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraTest {
+
+    private Calculadora calculadora;
+
+    @BeforeEach
+    void setUp() {
+        calculadora = new Calculadora();
+    }
+
+    @AfterEach
+    void tearDown() {
+        calculadora = null;
+    }
 
     @Test
     void suma() {
